20b984a82bc3f3da0579f242d0d5666fd6007d22
WILD07 <seg.wild07@gmail.com>
2018-09-14 00:20:28 -0400

    Primera etapa del prototipo

---
+++ Carreras/Assets/Scripts/ControlJugador.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ControlJugador : MonoBehaviour {
+
+    public float velocidad = 10;
+    float vertical;
+    float horizontal;
+    Rigidbody miRigidbody;
+    Vector3 posicionInicial;    
+    public Text textoVictoria;    
+    bool saliste;
+    public float fuerzaSalto;
+
+    // Use this for initialization
+    void Start()
+    {
+        miRigidbody = GetComponent<Rigidbody>();
+        posicionInicial = transform.position;
+        textoVictoria.enabled = false;
+        saliste = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!saliste)
+        {
+            vertical = Input.GetAxis("Vertical");
+            horizontal = Input.GetAxis("Horizontal");
+
+            miRigidbody.AddForce(new Vector3(horizontal, 0, vertical) * velocidad);
+            miRigidbody.AddForce(new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0, CrossPlatformInputManager.GetAxis("Vertical")) * velocidad);
+
+        }
+        if (CrossPlatformInputManager.GetButton("Jump") && Mathf.Abs(miRigidbody.velocity.y) < 0.01f)
+            miRigidbody.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            saliste = true;
+            textoVictoria.enabled = true;
+            miRigidbody.velocity = Vector3.zero;
+            miRigidbody.angularVelocity = Vector3.zero;
+        }           
+    }
+}
---
+++ Carreras/Assets/Scripts/MovNPC.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovNPC : MonoBehaviour {
+
+    [SerializeField]
+    Transform _destination;
+    UnityEngine.AI.NavMeshAgent _navMeshAgent;
+
+    // Use this for initialization
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("El agente de navegacion no esta añadido" + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+    }
+    private void SetDestination()
+    {
+        Vector3 targetVector = _destination.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
